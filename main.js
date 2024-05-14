276512802622489d7a3e4796dd78258aab0b139c
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-05-14 10:03:56 +0800

    123

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('hello word')
+import {Hexgate as HttpsClient, LcuClient as WsClient} from "hexgate";
+import {auth,poll} from "hexgate";
+
+const credentials=await poll(auth)
+console.log(credentials)
