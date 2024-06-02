46e8924b7a37b9428bcbdbbb2b7c61c1bc86b2af
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-06-02 20:31:37 +0800

    嘻嘻哈哈
    用react重构了！发现需要保存,git文件夹才能让github desktop识别

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ function createWindow() {
     let mainWin = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            nodeIntegration: true//允许在渲染进程中使用node
+        }
     })
-    mainWin.loadFile('.on(index.html')
-    mainWin("close", () => {
+    mainWin.loadFile('index.html')
+    mainWin.on("close", () => {
         console.log("主窗口已关闭")
         mainWin=null//赋值为空
     })
 }
 
 app.on("ready", createWindow)//监听app打开事件，直接调用创建窗口函数
+app.on("window-all-closed", ()=>{
+    console.log('所有窗口都关闭啦，下面退出程序')
+    app.quit()
+})
